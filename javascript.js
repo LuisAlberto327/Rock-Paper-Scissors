94bd113277d3a8b5d7153d5646058d334ddb3064
Luis327 <172340222+LuisAlberto327@users.noreply.github.com>
2024-10-06 02:43:01 -0600

    User Interface added  and new rules An user interface was added The player choice is now given by the buttons of the weapons Now the champion is the first winner of 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,64 +1,72 @@
+let playerPoints = 0;
+let computerPoints = 0;
+let roundCounter = 1;
+let computerChoice;
+let playerChoice;
+
+const round = document.querySelector("#round");
+const dialogue = document.querySelector("#dialogue");
+const scorePlayerOne = document.querySelector("#scorePlayerOne");
+const computerScore = document.querySelector("#computerScore");
+const buttons = document.querySelector("#buttons");
+
 function getComputerChoice() {
-    let random = (Math.floor(Math.random()*3));
-    if (random < 1){random = "rock";}
-    else if (random < 2) {random = "paper";}
-    else{random = "scissors";}
-    console.log("Computer choice:"+" "+random)
-    return random;
+  computerChoice = (Math.floor(Math.random()*3));
+  if (computerChoice < 1){computerChoice = "rock";}
+  else if (computerChoice < 2) {computerChoice = "paper";}
+  else{computerChoice = "scissors";}
+}
+
+buttons.addEventListener("click",()=>{
+  if(event.target.id=="rockButton"&&playerPoints<5&&computerPoints<5){
+    playerChoice = "rock";
+    getComputerChoice();
+    playRound(computerChoice, playerChoice);
   }
-  
-  function getHumanChoice(){
-      let pick;
-      do{
-          pick = prompt("Write: \"rock\", \"paper\" or \"scissors\"");
-          if(pick!=null){pick = pick.toLowerCase();}
-      }
-      while(pick!="rock"&&pick!="paper"&&pick!="scissors");
-      console.log("Human choice:"+" "+pick);
-      return pick;
+  else if(event.target.id=="paperButton"&&playerPoints<5&&computerPoints<5){
+    playerChoice = "paper";
+    getComputerChoice();
+    playRound(computerChoice, playerChoice);
   }
+  else if(event.target.id=="scissorsButton"&&playerPoints<5&&computerPoints<5){
+    playerChoice = "scissors";
+    getComputerChoice();
+    playRound(computerChoice, playerChoice);
+  }
+ 
+});
   
   function playRound(computer,human){
-      console.log("round:" + " " + round);
       if (computer == human){
-          console.log("draw game");
-          console.log("computer score:"+computerScore +" "+ "human score:"+humanScore);
-  }
+        dialogue.textContent = "It's a draw!";
+      }
       else if (computer == "rock" && human == "scissors" ||
                computer == "paper" && human == "rock" ||
                computer == "scissors" && human == "paper"){
-          computerScore = computerScore + 1;
-          console.log("computer wins");
-          console.log("computer score:"+computerScore +" "+ "human score:"+humanScore);
-  }
-      else {
-          humanScore = humanScore + 1;
-          console.log("human wins");
-          console.log("computer score:"+computerScore +" "+ "human score:"+humanScore);
+          computerPoints = computerPoints + 1;
+          dialogue.textContent = "Computer wins!";
       }
-      round = round + 1;
+      else {
+          playerPoints = playerPoints + 1;
+          dialogue.textContent = "Player 1 wins!";
+      }
+      roundCounter = roundCounter + 1;
+      round.textContent = "Round: "+roundCounter;
+      scorePlayerOne.textContent = playerPoints;
+      computerScore.textContent = computerPoints;
+      console.log(roundCounter);
+      if(playerPoints>4){
+         dialogue.innerHTML = `<p>You are the champion!</p> 
+         <p>Refresh the page to play again</p>`;
+         const p = document.querySelectorAll("p");
+         p[0].style.margin = 0;
+         p[1].style.margin = 0;
+      }
+      if(computerPoints>4){
+         dialogue.innerHTML = `<p>You are death!</p> 
+         <p>Refresh the page to play again</p>`;
+         const p = document.querySelectorAll("p");
+         p[0].style.margin = 0;
+         p[1].style.margin = 0;
+      }
   }
-  
-  let humanScore = 0;
-  let computerScore = 0;
-  let round = 1;
-  
-  
-  do{
-      const humanSelection = getHumanChoice();
-      const computerSelection = getComputerChoice();
-      playRound(computerSelection,humanSelection);
-  }
-  while(round<=5);
-  
-  if(round == 6 && humanScore>computerScore){
-    console.log("You are the campion (reload the page fo play again)");
-  
-   }else if(round == 6 && humanScore<computerScore){
-    console.log("You lost (reload the page fo play again)");
-      
-   } else{
-    console.log("Draw game (reload the page fo play again)");
-  }
-  
-  
